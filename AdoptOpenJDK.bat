82f0f05b021af64b2e2fb4d496c5adfc58f2b102
adam-lucero <48936972+adam-lucero@users.noreply.github.com>
2019-05-09 20:57:31 -0700

    Update AdoptOpenJDK.bat

---
+++ AdoptOpenJDK.bat
@@ -0,0 +1,20 @@
+@echo off
+
+
+
+
+for /f "tokens=3*" %%A in (
+  'Reg Query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" /s /v UninstallString ^| findstr 26A24AE4-039D-4CA4-87B4-2F'
+) do (
+  ECHO %%A %%B
+  %%A %%B /quiet /norestart
+)
+)
+
+for /f "tokens=3*" %%C in (
+  'Reg Query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /s /v UninstallString ^| findstr 26A24AE4-039D-4CA4-87B4-2F'
+) do (
+  ECHO %%C %%D
+  %%C %%D /quiet /norestart
+)
+)
