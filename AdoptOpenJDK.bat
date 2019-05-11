49480789e82ec52c90941b127118fcc1454ee983
adam-lucero <48936972+adam-lucero@users.noreply.github.com>
2019-05-10 21:31:45 -0700

    Update AdoptOpenJDK.bat

--- AdoptOpenJDK.bat
+++ AdoptOpenJDK.bat
@@ -12,44 +12,31 @@ IF '%ERRORLEVEL%'=='0' (SET jdkFiles=Yes)
 
 Reg Query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName | find "Java"
 IF '%ERRORLEVEL%'=='0' (
-   set jreEightOne=Yes
-   set upgradeJRE=Yes
+   set javaRegFiles=Yes
 )
 Reg Query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName | find "Java"
 IF '%ERRORLEVEL%'=='0' (
-   set jreEightOne=Yes
-   set upgradeJRE=Yes
+   set javaRegFiles=Yes
 )
 Reg Query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName | find "SE Development Kit"
 IF '%ERRORLEVEL%'=='0' (
-   set upgradeJDK=Yes
+   set jdkRegFiles=Yes
 )
 Reg Query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName | find "SE Development Kit"
 IF '%ERRORLEVEL%'=='0' (
-   set upgradeJDK=Yes
+   set jdkRegFiles=Yes
 )
 
 
-IF "%newJreFiles%"=="Yes" (
-   IF "%jreEightNew%"=="Yes" (
-      IF "%upgradeVerify%"=="Yes" (
-         IF "%upgradeJRE%"=="Yes" (
-            ECHO --- Upgrade Success but old JRE found ---
-            SET JREremediation=Yes
-            GOTO :jreRemediation            
-         )
-         IF "%remediateJDK%"=="Yes" (
-            ECHO --- Upgrade Success but old JDK found ---
-            SET JREremediation=Yes
-            GOTO :jreRemediation            
-         )          
-         ECHO --- Upgrade SUCCESS ---
-         GOTO :eof
-      )
-      ECHO --- Newest Java already installed, removing old ---
-      SET JREremediation=Yes
-      GOTO :jreRemediation      
-   )    
+IF "%jreFiles%"=="Yes" (
+  IF "%javaRegFiles%"=="Yes" (
+    ECHO Uninstall
+  )
+)
+IF "%jdkFiles%"=="Yes" (
+  IF "%jdkRegFiles%"=="Yes" (
+    ECHO Uninstall
+  )
 )
 
 
@@ -57,26 +44,23 @@ for /f "tokens=3*" %%A in (
   'Reg Query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" /s /v UninstallString ^| findstr 26A24AE4-039D-4CA4-87B4-2F'
 ) do (
   ECHO %%A %%B
+  )
 )
-)
-
 for /f "tokens=3*" %%C in (
   'Reg Query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /s /v UninstallString ^| findstr 26A24AE4-039D-4CA4-87B4-2F'
 ) do (
   ECHO %%C %%D
+  )
 )
-)
-
 for /f "tokens=3*" %%A in (
   'Reg Query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" /s /v UninstallString ^| findstr 3248F0A8-6813-11D6-A77B-00B0D01'
 ) do (
   ECHO %%A %%B
+  )
 )
-)
-
 for /f "tokens=3*" %%C in (
   'Reg Query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /s /v UninstallString ^| findstr 3248F0A8-6813-11D6-A77B-00B0D01'
 ) do (
   ECHO %%C %%D
-)
+  )
 )
