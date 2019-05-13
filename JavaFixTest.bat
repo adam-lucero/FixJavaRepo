495ff4a0b7d91946c664e8dfd4fee2da66f7a544
adam-lucero <48936972+adam-lucero@users.noreply.github.com>
2019-05-13 15:41:07 -0700

    Update JavaFixTest.bat

--- JavaFixTest.bat
+++ JavaFixTest.bat
@@ -1,17 +1,8 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
 
-set x86GUID=HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
-for /f "tokens=2*" %%A in (
-  'reg query "%x86GUID%" /V /F DisplayName /S /E ^| findstr "DisplayName"'
+for /f "tokens=3*" %%A in (
+  'Reg Query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName ^| findstr Java'
 ) do (
-  for /f "delims=" %%P in ('reg query "%x86GUID%" /s /f "%%B" ^| findstr "HKEY_LOCAL_MACHINE"') do (
-    for /f "tokens=2*" %%X in (
-      'reg query "%%P" /v "UninstallString" 2^>nul ^|findstr "UninstallString" ^| find "Java"'
-    ) do (
-      set MsiStr=%%Y
-      set MsiStr=!MsiStr:/I=/X!
-      echo !MsiStr! %%B
-    )
-  )
-)
+  ECHO %%A %%B
+ )
