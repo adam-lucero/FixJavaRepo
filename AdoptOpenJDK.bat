7e4ed239626bad7c164dafe60bd8d1c07a44761a
adam-lucero <48936972+adam-lucero@users.noreply.github.com>
2019-05-13 18:14:01 -0700

    Updating for speed

--- AdoptOpenJDK.bat
+++ AdoptOpenJDK.bat
@@ -1,66 +1,64 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
+set programFiles=No
+set regFiles=No
 
-DIR "C:\Program Files\Java" | FIND "jre"
-IF '%ERRORLEVEL%'=='0' (SET jreFiles=Yes)
-DIR "C:\Program Files (x86)\Java" | FIND "jre"
-IF '%ERRORLEVEL%'=='0' (SET jreFiles=Yes)
-DIR "C:\Program Files\Java" | FIND "jdk"
-IF '%ERRORLEVEL%'=='0' (SET jdkFiles=Yes)
-DIR "C:\Program Files (x86)\Java" | FIND "jdk"
-IF '%ERRORLEVEL%'=='0' (SET jdkFiles=Yes)
-
-
+:verification
+DIR "C:\Program Files\Java" | FIND "j"
+IF '%ERRORLEVEL%'=='0' (SET programFiles=Yes)
+DIR "C:\Program Files (x86)\Java" | FIND "j"
+IF '%ERRORLEVEL%'=='0' (SET programFiles=Yes)
 Reg Query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName | find "Java"
-IF '%ERRORLEVEL%'=='0' (
-   set javaRegFiles=Yes
-)
+IF '%ERRORLEVEL%'=='0' (set regFiles=Yes)
 Reg Query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName | find "Java"
-IF '%ERRORLEVEL%'=='0' (
-   set javaRegFiles=Yes
-)
-Reg Query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName | find "SE Development Kit"
-IF '%ERRORLEVEL%'=='0' (
-   set jdkRegFiles=Yes
-)
-Reg Query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName | find "SE Development Kit"
-IF '%ERRORLEVEL%'=='0' (
-   set jdkRegFiles=Yes
-)
+IF '%ERRORLEVEL%'=='0' (set regFiles=Yes)
 
 
-IF "%jreFiles%"=="Yes" (
-  IF "%javaRegFiles%"=="Yes" (
-    ECHO Uninstall
+
+IF "%programFiles%"=="Yes" (
+  IF "%regFiles%"=="Yes" (
+    GOTO :action
+  )
+) ELSE (GOTO :eof)
+
+:action
+set x86GUID=HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
+for /f "tokens=2*" %%A in (
+  'reg query "%x86GUID%" /V /F DisplayName /S /E 2^>nul ^| findstr "Java"'
+) do (
+  for /f "delims=" %%I in ('reg query "%x86GUID%" /s /f "%%B" 2^>nul ^| findstr "HKEY_LOCAL_MACHINE"') do (
+    for /f "tokens=2*" %%M in (
+      'reg query "%%I" /v "UninstallString" 2^>nul ^|findstr "UninstallString" ^|findstr "MsiExec.exe"'
+    ) do (
+      SET uninstallStr=%%N
+      SET uninstallStr=!uninstallStr:/I=/X!
+      ECHO !uninstallStr!
+    )
   )
 )
-IF "%jdkFiles%"=="Yes" (
-  IF "%jdkRegFiles%"=="Yes" (
-    ECHO Uninstall
+set x86GUID=HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall
+for /f "tokens=2*" %%A in (
+  'reg query "%x86GUID%" /V /F DisplayName /S /E 2^>nul ^| findstr "Java"'
+) do (
+  for /f "delims=" %%I in ('reg query "%x86GUID%" /s /f "%%B" 2^>nul ^| findstr "HKEY_LOCAL_MACHINE"') do (
+    for /f "tokens=2*" %%M in (
+      'reg query "%%I" /v "UninstallString" 2^>nul ^|findstr "UninstallString" ^|findstr "MsiExec.exe"'
+    ) do (
+      SET uninstallStr=%%N
+      SET uninstallStr=!uninstallStr:/I=/X!
+      ECHO !uninstallStr!
+    )
   )
 )
+ECHO DELETE PROGRAM FILES HERE
+ECHO INSTALL AdoptOpenJDK HERE
 
 
-for /f "tokens=3*" %%A in (
-  'Reg Query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" /s /v UninstallString ^| findstr 26A24AE4-039D-4CA4-87B4-2F'
-) do (
-  ECHO %%A %%B
-  )
-)
-for /f "tokens=3*" %%C in (
-  'Reg Query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /s /v UninstallString ^| findstr 26A24AE4-039D-4CA4-87B4-2F'
-) do (
-  ECHO %%C %%D
-  )
-)
-for /f "tokens=3*" %%A in (
-  'Reg Query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" /s /v UninstallString ^| findstr 3248F0A8-6813-11D6-A77B-00B0D01'
-) do (
-  ECHO %%A %%B
-  )
-)
-for /f "tokens=3*" %%C in (
-  'Reg Query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /s /v UninstallString ^| findstr 3248F0A8-6813-11D6-A77B-00B0D01'
-) do (
-  ECHO %%C %%D
-  )
-)
+DIR "C:\Program Files\AdoptOpenJDK" | FIND "j"
+IF '%ERRORLEVEL%'=='0' (SET adoptjdkFiles=Yes)
+DIR "C:\Program Files (x86)\AdoptOpenJDK" | FIND "j"
+IF '%ERRORLEVEL%'=='0' (SET adoptjdkFiles=Yes)
+
+
+
+:eof
--- JavaFixTest.bat
+++ JavaFixTest.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
 
+
+
 set x86GUID=HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
 for /f "tokens=2*" %%A in (
   'reg query "%x86GUID%" /V /F DisplayName /S /E 2^>nul ^| findstr "Java"'
@@ -9,7 +11,25 @@ for /f "tokens=2*" %%A in (
     for /f "tokens=2*" %%M in (
       'reg query "%%I" /v "UninstallString" 2^>nul ^|findstr "UninstallString" ^|findstr "MsiExec.exe"'
     ) do (
-      echo %%N
+      SET uninstallStr=%%N
+      SET uninstallStr=!uninstallStr:/I=/X!
+      ECHO !uninstallStr!
     )
   )
 )
+::
+set x86GUID=HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall
+for /f "tokens=2*" %%A in (
+  'reg query "%x86GUID%" /V /F DisplayName /S /E 2^>nul ^| findstr "Java"'
+) do (
+  for /f "delims=" %%I in ('reg query "%x86GUID%" /s /f "%%B" 2^>nul ^| findstr "HKEY_LOCAL_MACHINE"') do (
+    for /f "tokens=2*" %%M in (
+      'reg query "%%I" /v "UninstallString" 2^>nul ^|findstr "UninstallString" ^|findstr "MsiExec.exe"'
+    ) do (
+      SET uninstallStr=%%N
+      SET uninstallStr=!uninstallStr:/I=/X!
+      ECHO !uninstallStr!
+    )
+  )
+)
+
