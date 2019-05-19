c3b58ea58787a436bac558a9e0de31859c14830c
adam-lucero <48936972+adam-lucero@users.noreply.github.com>
2019-05-18 18:50:02 -0700

    Update AdoptOpenJDK.bat

--- AdoptOpenJDK.bat
+++ AdoptOpenJDK.bat
@@ -3,7 +3,8 @@ setlocal ENABLEDELAYEDEXPANSION
 set programFiles=No
 set regFiles=No
 
-:verification
+ECHO --- Starting Verification ---
+:: Verify Java is installed
 DIR "C:\Program Files\Java" | FIND "j"
 IF '%ERRORLEVEL%'=='0' (SET programFiles=Yes)
 DIR "C:\Program Files (x86)\Java" | FIND "j"
@@ -12,28 +13,23 @@ Reg Query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
 IF '%ERRORLEVEL%'=='0' (set regFiles=Yes)
 Reg Query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName | find "Java"
 IF '%ERRORLEVEL%'=='0' (set regFiles=Yes)
+:: 
+IF "%programFiles%"=="No" (GOTO :eof)
+IF "%regFiles%"=="No" (GOTO :eof)
 
 
-
-IF "%programFiles%"=="Yes" (
-  IF "%regFiles%"=="Yes" (
-    GOTO :cleanup
-  )
-) ELSE (GOTO :eof)
-
-:cleanup
 ECHO --- Removing commercial Java ---
-set x86GUID=HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
+set x64GUID=HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
 for /f "tokens=2*" %%A in (
-  'reg query "%x86GUID%" /V /F DisplayName /S /E 2^>nul ^| findstr "Java"'
+  'reg query "%x64GUID%" /V /F DisplayName /S /E 2^>nul ^| findstr "Java"'
 ) do (
-  for /f "delims=" %%I in ('reg query "%x86GUID%" /s /f "%%B" 2^>nul ^| findstr "HKEY_LOCAL_MACHINE"') do (
+  for /f "delims=" %%I in ('reg query "%x64GUID%" /s /f "%%B" 2^>nul ^| findstr "HKEY_LOCAL_MACHINE"') do (
     for /f "tokens=2*" %%M in (
       'reg query "%%I" /v "UninstallString" 2^>nul ^|findstr "UninstallString" ^|findstr "MsiExec.exe"'
     ) do (
       SET uninstallStr=%%N
       SET uninstallStr=!uninstallStr:/I=/X!
-      ECHO !uninstallStr!
+      !uninstallStr! /quiet /norestart
     )
   )
 )
@@ -47,16 +43,14 @@ for /f "tokens=2*" %%A in (
     ) do (
       SET uninstallStr=%%N
       SET uninstallStr=!uninstallStr:/I=/X!
-      ECHO !uninstallStr!
+      !uninstallStr! /quiet /norestart
     )
   )
 )
-
 ECHO --- Removing Program Files ---
-RMDIR /S /Q "C:\Program Files\Java\jre7"
-RMDIR /S /Q "C:\Program Files (x86)\Java\jre7"
-RMDIR /S /Q "C:\Program Files\Java\jre6"
-RMDIR /S /Q "C:\Program Files (x86)\Java\jre6"
+RMDIR /S /Q "C:\Program Files\Java\"
+RMDIR /S /Q "C:\Program Files (x86)\Java\"
+
 
 DIR "C:\Program Files\AdoptOpenJDK" | FIND "j"
 IF '%ERRORLEVEL%'=='0' (SET adoptjdkFiles=Yes)
