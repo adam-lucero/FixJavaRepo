b02a033ab84b0e0c9d180a6cfa3bc22fc78b3578
adam-lucero <48936972+adam-lucero@users.noreply.github.com>
2019-05-28 21:01:50 -0700

    Update JavaScript.bat

--- JavaScript.bat
+++ JavaScript.bat
@@ -4,19 +4,20 @@
 
 ::  Author: @dam Lucer0
 
-:: Instructions 
-:: 1. Make sure all devices have UAC Disabled (can be done with reg key)
-:: 2. Change the INSTALLATION PATH below!
+:: IMPORTANT
+:: 1. Only works with Windows
+:: 2. Doesn't run unless Java is detected in Program Files and the Registry 
+:: 3. Disable UAC before running script (can be done via Registry)
+:: 4. Change the INSTALLATION PATH below!
 
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
 set programFiles=No
 set regFiles=No
 
-
 :: Verify Java is installed
 :verify
-ECHO --- Starting Verification ---
+ECHO --- Verification ---
 DIR "C:\Program Files\Java" | FIND "j"
 IF '%ERRORLEVEL%'=='0' (SET programFiles=Yes)
 DIR "C:\Program Files (x86)\Java" | FIND "j"
@@ -29,25 +30,23 @@ IF '%ERRORLEVEL%'=='0' (set regFiles=Yes)
 IF "%programFiles%"=="No" (GOTO :eof)
 IF "%regFiles%"=="No" (GOTO :eof)
 
-:: Make sure Java 8 Update 201 is installed before continuing 
-ECHO --- Finding the latest version  ---
+:: Make sure Java 8 Update 201 is installed before continuing
 DIR "C:\Program Files\Java\jre1.8.0_201\bin\java.exe" 1>nul
 IF '%ERRORLEVEL%'=='0' (GOTO :cleanup)
 DIR "C:\Program Files (x86)\Java\jre1.8.0_201\bin\java.exe" 1>nul
 IF '%ERRORLEVEL%'=='0' (GOTO :cleanup)
 IF "%verifyUpgrade%"=="Yes" (GOTO :eof)
 
-:: CHANGE INSTALLATION PATH HERE !!!
+:: CHANGE INSTALLATION PATH BELOW !!!
+ECHO --- Upgrading ---
 E:\Downloads\Java\JRE\jre-8u201-windows-i586.exe /s REMOVEOUTOFDATEJRES=1
-::
-ECHO --- Upgrade Complete ---
-ECHO --- Verifying Successful Upgrade ---
+ECHO --- Verifying Upgrade ---
 SET verifyUpgrade=Yes
 GOTO :verify
 
 :: Uninstall everything except JRE 8 Update 201 - x86 and x64
 :cleanup
-ECHO --- Starting The Cleanup ---
+ECHO --- Cleanup ---
 set x64GUID=HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
 for /f "tokens=2*" %%A in (
   'reg query "%x64GUID%" /V /F DisplayName /S /E 2^>nul ^| findstr "Java"'
