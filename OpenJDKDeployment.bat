90880d7375f576aaf1fefc5c5b2ff50d331bbfe0
adam-lucero <48936972+adam-lucero@users.noreply.github.com>
2019-06-24 21:53:19 -0700

    Update OpenJDKDeployment.bat

--- OpenJDKDeployment.bat
+++ OpenJDKDeployment.bat
@@ -17,21 +17,28 @@ SET installationPath=E:\Downloads\Java\AdoptOpenJDK\OpenJDK8U-jre_x86-32_windows
 
 
 SET installedJava=C:\Windows\Temp\InstalledJava.txt
-ECHO --- Begin Log --- >> %installedJava%
+ECHO --- Log Start --- >> %installedJava%
 TIME /T >> %installedJava%
 DATE /T >> %installedJava%
+ECHO --- Sytem Info BEFORE Changes --- >> %installedJava% 
 ECHO PATH Environment Variables: >> %installedJava%
 SET PATH >> %installedJava%
 
-ECHO --- Java Registry Keys  --- >> %installedJava%
+
+ECHO --- Registry Keys Found --- >> %installedJava%
 SET regFiles=No
 REG Query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName 2>nul | FIND "Java" >> %installedJava%
 IF '%ERRORLEVEL%'=='0' (SET regFiles=Yes)
 REG Query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName 2>nul | FIND "Java" >> %installedJava%
 IF '%ERRORLEVEL%'=='0' (SET regFiles=Yes)
-IF "%regFiles%"=="No" (GOTO :eof)
+IF "%regFiles%"=="No" (
+  ECHO --- No Registry Key Matches  ---
+  ECHO --- No Registry Key Matches --- >> %installedJava%
+  GOTO :eof
+)
 
-ECHO --- Java Program Files --- >> %installedJava%
+
+ECHO --- Program Files Found --- >> %installedJava%
 SET programFiles=No
 FOR /D %%C IN ("C:\Program Files\Java\*") DO (
     SET directory=%%C
@@ -45,11 +52,14 @@ FIND "C:\Program Files\Java\j" %installedJava% >> %installedJava%
 IF '%ERRORLEVEL%'=='0' (SET programFiles=Yes)
 FIND "C:\Program Files (x86)\Java\j" %installedJava% >> %installedJava%
 IF '%ERRORLEVEL%'=='0' (SET programFiles=Yes )
-IF "%programFiles%"=="No" (GOTO :eof)
-ECHO --- Java Detected ---
+IF "%programFiles%"=="No" (
+  ECHO --- No Program File Matches ---
+  ECHO --- No Program File Matches --- >> %installedJava%
+  GOTO :eof
+)
 
 
-ECHO --- Uninstalling ---
+ECHO --- Uninstalling Older Versions ---
 SET x64GUID=HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
 FOR /f "tokens=2*" %%A in (
   'REG query "%x64GUID%" /V /F DisplayName /S /E 2^>nul ^| FIND "Java"'
@@ -82,16 +92,31 @@ RMDIR /S /Q "C:\Program Files\Java\" 2>nul
 RMDIR /S /Q "C:\Program Files (x86)\Java\" 2>nul
 
 
+ECHO --- Sytem Info AFTER Changes --- >> %installedJava% 
+SET PATH >> %installedJava%
+REG Query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName 2>nul | FIND "Java" >> %installedJava%
+REG Query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName 2>nul | FIND "Java" >> %installedJava%
+FOR /D %%C IN ("C:\Program Files\Java\*") DO (
+    SET directory=%%C
+    DIR "!directory!" >> %installedJava%
+)
+FOR /D %%C IN ("C:\Program Files (x86)\Java\*") DO (
+    SET directory=%%C
+    DIR "!directory!" >> %installedJava%
+)
+
+
 :verifyUpgrade
 ECHO --- AdoptOpenJDK Installations --- >> %installedJava%
-DIR "C:\Program Files\AdoptOpenJDK" 2>nul | FIND /I "j" >> %installedJava%
+DIR "C:\Program Files\AdoptOpenJDK\" 2>nul | FIND /I "j" >> %installedJava%
 IF '%ERRORLEVEL%'=='0' (SET adoptjdkFiles=Yes)
-DIR "C:\Program Files (x86)\AdoptOpenJDK" 2>nul | FIND /I "j" >> %installedJava%
+DIR "C:\Program Files (x86)\AdoptOpenJDK\" 2>nul | FIND /I "j" >> %installedJava%
 IF '%ERRORLEVEL%'=='0' (SET adoptjdkFiles=Yes)
 
 IF "%adoptjdkFiles%"=="Yes" (
   IF "%verifyUpgrade%"=="Yes" (
     ECHO --- AdoptOpenJDK Installation Successful ---
+    ECHO --- AdoptOpenJDK Installation Successful --- >> %installedJava%
     GOTO :eof
   )
   GOTO :eof
@@ -99,7 +124,6 @@ IF "%adoptjdkFiles%"=="Yes" (
 IF "%verifyUpgrade%"=="Yes" (
   ECHO --- AdoptOpenJDK Installation Failure ---
   ECHO --- AdoptOpenJDK Installation Failure --- >> %installedJava%
-  SET PATH >> %installedJava%
   GOTO :eof
 )
 
