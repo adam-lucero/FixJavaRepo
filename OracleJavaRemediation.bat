a8f98bb45d47b3849ad8cd376fe09cac7b021e0a
adam-lucero <48936972+adam-lucero@users.noreply.github.com>
2019-06-25 22:13:14 -0700

    Update OracleJavaRemediation.bat

--- OracleJavaRemediation.bat
+++ OracleJavaRemediation.bat
@@ -1,5 +1,6 @@
-@echo off
-setlocal ENABLEDELAYEDEXPANSION
+@ECHO off
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET loggingVar=###################
 
 ::::::::::::::::::::::::::::
 ::  OracleJavaRemediation ::
@@ -11,46 +12,85 @@ setlocal ENABLEDELAYEDEXPANSION
 :: All target devices must have UAC Disabled before running (registry key)
 :: If Java is installed, script will silently upgrade to the latest version and remove all old versions (JDK and JRE) 
 
-:: CHANGE INSTALLATION PATH !!! 
-SET installationPath=\\Domain\NETLOGON\JRE8U201.exe
+:: Where is the installation media? 
+SET installationPath=E:\Downloads\Java\JRE\jre-8u201-windows-i586.exe
+:: What is the current version of Java? 
+SET currentVersion=Java 8 Update 201
+:: What are the 32 and 64 bit Uninstallstrings for the current version
+SET uninstallString=MsiExec.exe /X{26A24AE4-039D-4CA4-87B4-2F32180201F0}
+SET uninstallStringSixFour=MsiExec.exe /X{26A24AE4-039D-4CA4-87B4-2F64180201F0}
+:: What is the installation directory for the current version
+SET installDirectory=C:\Program Files (x86)\Java\jre1.8.0_201
+SET installDirectorySixFour=C:\Program Files\Java\jre1.8.0_201
 
-:: Silent and remove old installations (DEFAULT)
-SET switches=/s REMOVEOUTOFDATEJRES=1
+
+
+SET installedJava=C:\Windows\Temp\InstalledJava.txt
+DEL /F /Q %installedJava% 2>nul
+ECHO %loggingVar% START LOG %loggingVar% >> %installedJava%
+TIME /T >> %installedJava%
+DATE /T >> %installedJava% 
+ECHO %loggingVar% Before Changes %loggingVar% >> %installedJava%
+
 
 :verify
-ECHO --- Finding Java Installations ---
-set programFiles=No
-set regFiles=No
-DIR "C:\Program Files\Java" 2>nul | FIND "j"
+ECHO %loggingVar% Registry Keys %loggingVar% >> %installedJava%
+SET regFiles=No
+REG Query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName 2>nul | FIND "Java" >> %installedJava%
+IF '%ERRORLEVEL%'=='0' (SET regFiles=Yes)
+REG Query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName 2>nul | FIND "Java" >> %installedJava%
+IF '%ERRORLEVEL%'=='0' (SET regFiles=Yes)
+IF "%regFiles%"=="No" (
+  ECHO %loggingVar% ENDING: No Registry Key Matches %loggingVar% >> %installedJava%
+  GOTO :eof
+)
+ECHO %loggingVar% Program Files %loggingVar% >> %installedJava%
+SET programFiles=No
+FOR /D %%C IN ("C:\Program Files\Java\*") DO (
+    SET directory=%%C
+    ECHO !directory! >> %installedJava%
+    DIR "!directory!" >> %installedJava%
+)
+FOR /D %%C IN ("C:\Program Files (x86)\Java\*") DO (
+    SET directory=%%C
+    ECHO !directory! >> %installedJava%
+    DIR "!directory!" >> %installedJava%
+)
+IF "%cleanupComplete%"=="Yes" (
+  ECHO --- ENDING: Script Success ---
+  GOTO :eof
+)
+FIND "C:\Program Files\Java\j" %installedJava% >> %installedJava%
 IF '%ERRORLEVEL%'=='0' (SET programFiles=Yes)
-DIR "C:\Program Files (x86)\Java" 2>nul | FIND "j"
-IF '%ERRORLEVEL%'=='0' (SET programFiles=Yes)
-Reg Query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName 2>nul | find "Java"
-IF '%ERRORLEVEL%'=='0' (set regFiles=Yes)
-Reg Query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /s /v DisplayName 2>nul | find "Java"
-IF '%ERRORLEVEL%'=='0' (set regFiles=Yes)
-::
-IF "%programFiles%"=="No" (GOTO :eof)
-IF "%regFiles%"=="No" (GOTO :eof)
+FIND "C:\Program Files (x86)\Java\j" %installedJava% >> %installedJava%
+IF '%ERRORLEVEL%'=='0' (SET programFiles=Yes )
+IF "%programFiles%"=="No" (
+  ECHO %loggingVar% ENDING: No Program File Matches %loggingVar% >> %installedJava%
+  GOTO :eof
+)
 
 
-ECHO --- Verifying Java Version ---
-DIR "C:\Program Files\Java\jre1.8.0_201\bin\java.exe" 2>nul
-IF '%ERRORLEVEL%'=='0' (GOTO :cleanup)
-DIR "C:\Program Files (x86)\Java\jre1.8.0_201\bin\java.exe" 2>nul
-IF '%ERRORLEVEL%'=='0' (GOTO :cleanup)
-IF "%verifyUpgrade%"=="Yes" (GOTO :eof)
+FIND "%currentVersion%" %installedJava% >> %installedJava%
+IF '%ERRORLEVEL%'=='0' (
+    ECHO --- Current Version Detected ---
+    GOTO :cleanup
+)
+IF "%verifyUpgrade%"=="Yes" (
+    ECHO --- ENDING: Upgrade Failure ---
+    GOTO :eof
+)
 
 
-ECHO --- Upgrading Java ---
-%installationPath% %switches%
+ECHO --- Upgrading ---
+%installationPath% /s REMOVEOUTOFDATEJRES=1
 ECHO --- Verifying Upgrade ---
+ECHO %loggingVar% After Upgrade %loggingVar% >> %installedJava%
 SET verifyUpgrade=Yes
 GOTO :verify
 
-:: Uninstall everything except JRE 8 Update 201 - x86 and x64
+
 :cleanup
-ECHO --- Removing Old Java Versions ---
+ECHO --- Removing Old Versions ---
 set x64GUID=HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
 for /f "tokens=2*" %%A in (
   'reg query "%x64GUID%" /V /F DisplayName /S /E 2^>nul ^| findstr "Java"'
@@ -61,8 +101,8 @@ for /f "tokens=2*" %%A in (
     ) do (
       SET uninstallStr=%%N
       SET uninstallStr=!uninstallStr:/I=/X!
-      IF NOT "!uninstallStr!"=="MsiExec.exe /X{26A24AE4-039D-4CA4-87B4-2F32180201F0}" (
-        IF NOT "!uninstallStr!"=="MsiExec.exe /X{26A24AE4-039D-4CA4-87B4-2F64180201F0}" (
+      IF NOT "!uninstallStr!"=="%uninstallString%" (
+        IF NOT "!uninstallStr!"=="%uninstallStringSixFour%" (
           !uninstallStr! /quiet /norestart
         )
       )
@@ -79,30 +119,32 @@ for /f "tokens=2*" %%A in (
     ) do (
       SET uninstallStr=%%N
       SET uninstallStr=!uninstallStr:/I=/X!
-      IF NOT "!uninstallStr!"=="MsiExec.exe /X{26A24AE4-039D-4CA4-87B4-2F32180201F0}" (
-        IF NOT "!uninstallStr!"=="MsiExec.exe /X{26A24AE4-039D-4CA4-87B4-2F64180201F0}" (
+      IF NOT "!uninstallStr!"=="%uninstallString%" (
+        IF NOT "!uninstallStr!"=="%uninstallStringSixFour%" (
           !uninstallStr! /quiet /norestart
         )
       )
     )
   )
 )
-ECHO --- Removing Old Program Files ---
-FOR /D %%C IN (
-    "C:\Program Files\Java\*"
-) DO (
-    SET directory=%%C
-    IF NOT "!directory!"=="C:\Program Files\Java\jre1.8.0_201" (
-        RMDIR /S /Q "!directory!"
-    )
-)
 FOR /D %%C IN (
     "C:\Program Files (x86)\Java\*"
 ) DO (
     SET directory=%%C
-    IF NOT "!directory!"=="C:\Program Files (x86)\Java\jre1.8.0_201" (
+    IF NOT "!directory!"=="%installDirectory%" (
         RMDIR /S /Q "!directory!"
     )
 )
+FOR /D %%C IN (
+    "C:\Program Files\Java\*"
+) DO (
+    SET directory=%%C
+    IF NOT "!directory!"=="%installDirectorySixFour%" (
+        RMDIR /S /Q "!directory!"
+    )
+)
+SET cleanupComplete=Yes
+ECHO %loggingVar% After Cleanup %loggingVar% >> %installedJava%
+GOTO :verify
 
 :eof
