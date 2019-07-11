f6aad152b424dd026337ff1e82f74582f2bae4b0
adam-lucero <48936972+adam-lucero@users.noreply.github.com>
2019-07-10 21:16:58 -0700

    Update OracleJavaRemediation.bat

--- OracleJavaRemediation.bat
+++ OracleJavaRemediation.bat
@@ -12,26 +12,26 @@ SET loggingVar=###################
 :: MUST disable UAC on target devices before running script (registry key available)
 :: If Java is installed, script will silently upgrade to the latest version and remove all old versions (JDK and JRE) 
 
-:: Where is the installation media? 
+:: CHANGE INSTALLATION PATH HERE !!!
 SET installationPath=E:\Downloads\Java\JRE\jre-8u201-windows-i586.exe
-:: What is the current version of Java? 
+
+
+:: Variables for Version Control
 SET currentVersion=Java 8 Update 201
-:: What are the 32 and 64 bit Uninstallstrings for the current version
+:: 32 and 64 bit Uninstallstrings
 SET uninstallString=MsiExec.exe /X{26A24AE4-039D-4CA4-87B4-2F32180201F0}
 SET uninstallStringSixFour=MsiExec.exe /X{26A24AE4-039D-4CA4-87B4-2F64180201F0}
-:: What is the installation directory for the current version
+:: Installation Directory
 SET installDirectory=C:\Program Files (x86)\Java\jre1.8.0_201
 SET installDirectorySixFour=C:\Program Files\Java\jre1.8.0_201
 
 
-
+::
 SET installedJava=C:\Windows\Temp\InstalledJava.txt
 DEL /F /Q %installedJava% 2>nul
 TIME /T >> %installedJava%
 DATE /T >> %installedJava% 
 ECHO %loggingVar% Before Changes %loggingVar% >> %installedJava%
-
-
 :verify
 ECHO %loggingVar% Registry Keys %loggingVar% >> %installedJava%
 SET regFiles=No
@@ -59,6 +59,7 @@ IF "%cleanupComplete%"=="Yes" (
   ECHO --- ENDING: Script Success ---
   GOTO :eof
 )
+IF "%verifyUpgrade%"=="Yes" (GOTO :verifyVersion)
 FIND "C:\Program Files\Java\j" %installedJava% >> %installedJava%
 IF '%ERRORLEVEL%'=='0' (SET programFiles=Yes)
 FIND "C:\Program Files (x86)\Java\j" %installedJava% >> %installedJava%
@@ -68,7 +69,7 @@ IF "%programFiles%"=="No" (
   GOTO :eof
 )
 
-
+:verifyVersion
 FIND "%currentVersion%" %installedJava% >> %installedJava%
 IF '%ERRORLEVEL%'=='0' (
     ECHO --- Current Version Detected ---
@@ -91,12 +92,12 @@ GOTO :verify
 :cleanup
 ECHO --- Removing Old Versions ---
 set x64GUID=HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
-for /f "tokens=2*" %%A in (
-  'reg query "%x64GUID%" /V /F DisplayName /S /E 2^>nul ^| findstr "Java"'
+FOR /f "tokens=2*" %%A in (
+  'reg query "%x64GUID%" /V /F DisplayName /S /E 2^>nul ^| FIND "Java"'
 ) do (
-  for /f "delims=" %%I in ('reg query "%x64GUID%" /s /f "%%B" 2^>nul ^| findstr "HKEY_LOCAL_MACHINE"') do (
-    for /f "tokens=2*" %%M in (
-      'reg query "%%I" /v "UninstallString" 2^>nul ^|findstr "UninstallString" ^|findstr "MsiExec.exe"'
+  FOR /f "delims=" %%I in ('reg query "%x64GUID%" /s /f "%%B" 2^>nul ^| FIND "HKEY_LOCAL_MACHINE"') do (
+    FOR /f "tokens=2*" %%M in (
+      'reg query "%%I" /v "UninstallString" 2^>nul ^| FIND "UninstallString" ^| FIND "MsiExec.exe"'
     ) do (
       SET uninstallStr=%%N
       SET uninstallStr=!uninstallStr:/I=/X!
@@ -109,12 +110,12 @@ for /f "tokens=2*" %%A in (
   )
 )
 set x86GUID=HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall
-for /f "tokens=2*" %%A in (
-  'reg query "%x86GUID%" /V /F DisplayName /S /E 2^>nul ^| findstr "Java"'
+FOR /f "tokens=2*" %%A in (
+  'reg query "%x86GUID%" /V /F DisplayName /S /E 2^>nul ^| FIND "Java"'
 ) do (
-  for /f "delims=" %%I in ('reg query "%x86GUID%" /s /f "%%B" 2^>nul ^| findstr "HKEY_LOCAL_MACHINE"') do (
-    for /f "tokens=2*" %%M in (
-      'reg query "%%I" /v "UninstallString" 2^>nul ^|findstr "UninstallString" ^|findstr "MsiExec.exe"'
+  FOR /f "delims=" %%I in ('reg query "%x86GUID%" /s /f "%%B" 2^>nul ^| FIND "HKEY_LOCAL_MACHINE"') do (
+    FOR /f "tokens=2*" %%M in (
+      'reg query "%%I" /v "UninstallString" 2^>nul ^| FIND "UninstallString" ^| FIND "MsiExec.exe"'
     ) do (
       SET uninstallStr=%%N
       SET uninstallStr=!uninstallStr:/I=/X!
